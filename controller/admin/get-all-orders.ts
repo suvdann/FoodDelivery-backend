480709954222c98f495437d4e64e178760150140
suvdann <m.erdenesuwd2021@gmail.com>
2025-07-24 15:29:40 +0800

    orders back

--- controller/admin/get-all-orders.ts
+++ controller/admin/get-all-orders.ts
@@ -2,14 +2,20 @@ import { Request, Response } from "express";
 import { FoodOrderModel } from "../../model/order.model";
 
 export const getAllOrders = async (_req: Request, res: Response) => {
-  const allOrders = await FoodOrderModel.find({})
-    .populate({
-      path: "foodOrderItems",
-      populate: {
-        path: "food",
-        model: "Foods",
-      },
-    })
-    .populate("user");
-  res.status(200).send({ orders: allOrders });
+  try {
+    const allOrders = await FoodOrderModel.find({})
+
+      .populate({
+        path: "foodOrderItems",
+        populate: {
+          path: "food",
+          model: "Foods",
+        },
+      })
+      .populate("user");
+
+    res.status(200).send({ orders: allOrders });
+  } catch (err) {
+    res.status(400).send({ message: "getAllOrders error" });
+  }
 };
